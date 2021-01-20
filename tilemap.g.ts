8a2407617584ab1288afa9049bc0cd983c8aee81
gaetanrebufet <76159070+gaetanrebufet@users.noreply.github.com>
2021-01-20 09:42:48 +0100

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,24 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`0a00070000000000000000000000000000000000000000000000010101010100000000000101010101000000000001010101010000000000000000000000000000000000000000000000`, img`
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 . . . . . 2 2 2 
+2 2 . . . . . 2 2 2 
+2 2 . . . . . 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile1], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
